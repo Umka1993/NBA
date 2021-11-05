d06f02446ef6a9dbc2a0177800bc856259567feb
Ivan <vanish475@gmail.com>
2021-11-05 11:18:53 +0200

    remade folder tree

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AuthPage from "./pages/authorization/authPage";
+
+const App = () => {
+    return (
+        <div>
+            <AuthPage/>
+        </div>
+    );
+};
+
+export default App;
+
+
