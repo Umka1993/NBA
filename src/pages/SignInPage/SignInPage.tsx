d90dbcbf0e2f727d1a6ed71c6c78cfbf0bed6d0a
Ivan <vanish475@gmail.com>
2021-12-06 14:08:46 +0200

    Add Unauthorized Error

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import s from './SignInPage.module.scss';
 import { SignInForm } from './components/SignInForm';
+import { useAppSelector } from '../../core/redux/hooks/redux';
 
 export const SignInPage = (): JSX.Element => {
+  const { error, isLoading, message } = useAppSelector((state) => state.loginSliceReducer);
+  debugger;
   return (
-    <div className={s.wrap}>
-      <div className={s.colLeft}>
-        <div className={s.colLeftWrap}>
-          <div className={s.colWrap}>
-            <SignInForm />
+    <>
+      {isLoading && (
+        <div className={s.loading}>
+          <h2>Loading... </h2>
+        </div>
+      )}
+
+      {!isLoading && (
+        <div className={s.wrap}>
+          {error && (
+            <div className={s.error}>
+              <span>{message}</span>
+            </div>
+          )}
+
+          <div className={s.colLeft}>
+            <div className={s.colLeftWrap}>
+              <div className={s.colWrap}>
+                <SignInForm />
+              </div>
+            </div>
+          </div>
+          <div className={s.colRight}>
+            <div className={s.colWrap}></div>
           </div>
         </div>
-      </div>
-      <div className={s.colRight}>
-        <div className={s.colWrap}></div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
